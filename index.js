5ce3a84c58f25f5843b23e8e661dddd08d90eecb
Mahmudul Hasan <17101120@uap-bd.edu>
2021-10-23 12:33:25 +0600

    post data recive

--- index.js
+++ index.js
@@ -1,27 +1,57 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+// use 
+app.use(cors());
+app.use(express.json());
+
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+    res.send('Hello world');
+});
+
 const users = [
-    { id: 1, name: "Akash", roll: "153", ht: "Chapai" },
-    { id: 2, name: "Anik", roll: "153", ht: "Chapai" },
-    { id: 3, name: "Fahim", roll: "153", ht: "Chapai" },
-    { id: 4, name: "Urmi", roll: "153", ht: "Chapai" },
-    { id: 5, name: "Khosru", roll: "153", ht: "Chapai" },
-    { id: 6, name: "showmik", roll: "153", ht: "Chapai" },
+    { id: 0, name: 'Shabana', email: 'Shabana@gmail.com', phone: '01788888888' },
+    { id: 1, name: 'Shabnoor', email: 'Shabnoor@gmail.com', phone: '01788888888' },
+    { id: 2, name: 'Shrabonti', email: 'Shrabonti@gmail.com', phone: '01788888888' },
+    { id: 3, name: 'Suchorita', email: 'Suchorita@gmail.com', phone: '01788888888' },
+    { id: 4, name: 'Soniya', email: 'Soniya@gmail.com', phone: '01788888888' },
+    { id: 5, name: 'Susmita', email: 'Susmita@gmail.com', phone: '01788888888' },
 ]
+
 app.get('/users', (req, res) => {
-    // res.send(users) eta 
+    const search = req.query.search;
+    // use query parameter
+    if (search) {
+        const searchResult = users.filter(user => user.name.toLocaleLowerCase().includes(search));
+        res.send(searchResult);
+    }
+    else {
+        res.send(users)
+    }
+});
+
+// app.METHOD
+app.post('/users', (req, res) => {
+    console.log(req.body)
 })
+
+// dynamic api
 app.get('/users/:id', (req, res) => {
     const id = req.params.id;
-    const selecteduser = users[id];
-    res.send(selecteduser);
+    const user = users[id]
+    res.send(user);
+})
+
+app.get('/fruits', (req, res) => {
+    res.send(['mango', 'oranges', 'banana', 'apple'])
+})
+
+app.get('/fruits/mangoes/fazli', (req, res) => {
+    res.send('Yummy Yummy tok marka fazli');
 })
 
 app.listen(port, () => {
-    console.log('lisiting to port', port)
+    console.log('Listening to port ', port);
 })
